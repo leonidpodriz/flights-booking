1291d8be8e0132f1e6b872f4ca671a22488fa447
Leonid Podriz <leonidpodriz@gmail.com>
2021-03-27 14:35:37 +0200

    :white_check_mark: Add tests for Callback class

--- src/main/java/Console/Callback.java
+++ src/main/java/Console/Callback.java
@@ -3,25 +3,38 @@ package Console;
 import java.util.function.Consumer;
 
 public class Callback {
-    String name = "Default callback";
-    Consumer<Console> toRun;
+    static final String RUNNER_NOT_PROVIDED_EXCEPTION_MESSAGE = "Runner was not provided!";
+    static final String DEFAULT_NAME = "Callback";
+    static final Consumer<Console> DEFAULT_RUNNER = console -> {
+        throw new RuntimeException(RUNNER_NOT_PROVIDED_EXCEPTION_MESSAGE);
+    };
 
-    public Callback(String name) {
-        this.name = name;
-    }
+    final String name;
+    final Consumer<Console> runner;
 
     public Callback(String name, Consumer<Console> toRun) {
-        this(name);
-        this.toRun = toRun;
+        this.name = name;
+        this.runner = toRun;
+    }
+
+    public Callback(String name) {
+        this(name, DEFAULT_RUNNER);
     }
 
     public Callback() {
+        this(DEFAULT_NAME, DEFAULT_RUNNER);
+    }
+
+    private boolean isToRunPresent() {
+        return runner != null;
+    }
+
+    private void acceptToRunner(Console console) {
+        runner.accept(console);
     }
 
     public void run(Console console) {
-        if (toRun != null) {
-            toRun.accept(console);
-        }
+        if (isToRunPresent()) acceptToRunner(console);
     }
 
     @Override
