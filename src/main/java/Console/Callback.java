0b3253ae5b0b4fbb1defa326561781e4e433f2ea
Leonid Podriz <leonidpodriz@gmail.com>
2021-03-27 12:31:41 +0200

    :art: Improve Callback behaviour

--- src/main/java/Console/Callback.java
+++ src/main/java/Console/Callback.java
@@ -1,11 +1,31 @@
 package Console;
 
+import java.util.function.Consumer;
+
 public class Callback {
+    String name = "Default callback";
+    Consumer<Console> toRun;
+
+    public Callback(String name) {
+        this.name = name;
+    }
+
+    public Callback(String name, Consumer<Console> toRun) {
+        this.name = name;
+        this.toRun = toRun;
+    }
+
+    public Callback() {
+    }
+
     public void run(Console console) {
+        if (toRun != null) {
+            toRun.accept(console);
+        }
     }
 
     @Override
     public String toString() {
-        return "Default callback";
+        return name;
     }
 }
